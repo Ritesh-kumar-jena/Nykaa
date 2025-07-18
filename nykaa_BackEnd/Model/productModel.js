57fbd4187b0ce5c52ffd4d1aa9e135224015b701
Ritesh-kumar-jena <jenariteshkumar85@gmail.com>
2025-07-18 12:49:36 +0530

    Nykaa Backend

--- nykaa_BackEnd/Model/productModel.js
+++ nykaa_BackEnd/Model/productModel.js
@@ -4,7 +4,6 @@ const productSchema=mongoose.Schema({
     name:{type:String,required:true},
     category:{type:String,required:true},
     type:{type:String,required:true},
-    description:{type:String,required:true},
     price:{type:Number,required:true},
     image:{type:String,required:true}
 },{versionKey:false})
